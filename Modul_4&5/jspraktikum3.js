23d4675eb11a826ca92d19622a1f2990ecb813d8
abansybana09 <abansybana322@gmail.com>
2024-12-11 19:35:21 +0700

    initial commit

---
+++ Modul_4&5/jspraktikum3.js
@@ -0,0 +1,12 @@
+function pesan() {
+    alert ("Ini Latihan Javasript pertama saya!");
+}
+
+function pesanConsole(){
+    console.log("It's work");
+}
+
+function pesanDok(){
+    document.write("Success<br>");
+    document.write("<a href='praktikum2.html' >Kembali</a>");
+}
